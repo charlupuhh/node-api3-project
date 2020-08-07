44c4117950eedff30ed14126471733f9e9671335
charlupuhh <62491397+charlupuhh@users.noreply.github.com>
2020-08-06 20:35:47 -0700

    port

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./server')
 
-server.listen(4000, ()=>{
+const port = process.env.PORT || 5000;
+
+server.listen(port, ()=>{
   console.log('server listening on port 4000');
 }) 
